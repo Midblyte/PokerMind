7b2d576f8b28528b2e700f520105dddce3ebfe6b
Mario A. <10923513+Midblyte@users.noreply.github.com>
2025-05-18 16:40:07 +0200

    Add training mode

--- src/client.py
+++ src/client.py
@@ -24,6 +24,8 @@ def main():
     reveal_parser = subparsers.add_parser("reveal")
     reveal_parser.set_defaults(action="reveal")
 
+    reveal_parser.add_argument("--training-mode", "-T", action="store_true", help="output as CSV and create a new game")
+
     guess_parser = subparsers.add_parser("guess")
     guess_parser.set_defaults(action="guess")
 
@@ -43,7 +45,12 @@ def main():
         case "show":
             ok, data = proxy.show()
         case "reveal":
-            ok, data = proxy.reveal()
+            if args.training_mode:
+                ok = training(proxy)
+
+                exit(0 if ok else 1)
+            else:
+                ok, data = proxy.reveal()
         case "new_game":
             ok, data = proxy.new_game()
         case "guess":
@@ -60,6 +67,53 @@ def main():
     print(json.dumps(data, indent=4))
 
     exit(0 if ok else 1)
+
+
+def training(proxy: Proxy) -> bool:
+    ok_reveal,   data_reveal   = proxy.reveal()
+    ok_show,     data_show     = proxy.show()
+    ok_new_game, data_new_game = proxy.new_game()
+
+    if not (ok_reveal and ok_show and ok_new_game):
+        return False
+
+    print(','.join(map(str, [
+        # Input
+
+        # Unrolled version:
+        #   data_show[player][n][kind]["value"]
+        #   for player in ("player1", "player2")
+        #   for n in range(4)
+        #   for kind in ("rank", "suit")
+
+        data_show["player1"][0]["rank"]["value"],
+        data_show["player1"][0]["suit"]["value"],
+        data_show["player1"][1]["rank"]["value"],
+        data_show["player1"][1]["suit"]["value"],
+        data_show["player1"][2]["rank"]["value"],
+        data_show["player1"][2]["suit"]["value"],
+        data_show["player1"][3]["rank"]["value"],
+        data_show["player1"][3]["suit"]["value"],
+
+        data_show["player2"][0]["rank"]["value"],
+        data_show["player2"][0]["suit"]["value"],
+        data_show["player2"][1]["rank"]["value"],
+        data_show["player2"][1]["suit"]["value"],
+        data_show["player2"][2]["rank"]["value"],
+        data_show["player2"][2]["suit"]["value"],
+        data_show["player2"][3]["rank"]["value"],
+        data_show["player2"][3]["suit"]["value"],
+
+        data_show["round1"]["value"],
+        data_show["round2"]["value"],
+
+        # Output
+
+        data_reveal["card"]["rank"]["value"],
+        data_reveal["card"]["suit"]["value"]
+    ])))
+
+    return True
 
 
 if __name__ == "__main__":
