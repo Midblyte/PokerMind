2dcbc0fafc0952d4a37e9f1c12337997a784d65d
Mario A. <10923513+Midblyte@users.noreply.github.com>
2025-05-24 20:03:50 +0200

    Change types, fix typings, remove warning suppressions
    Change types, fix typings, remove warning suppressions

--- src/game/hand.py
+++ src/game/hand.py
@@ -7,35 +7,35 @@ from game.rank import Rank
 from game.suit import Suit
 
 
-_ROYAL_FLUSHES: dict[Suit, set[Card]] = {
-    Suit.HEARTS: {
+_ROYAL_FLUSHES: dict[Suit, tuple[Card, Card, Card, Card, Card]] = {
+    Suit.HEARTS: (
         Card(rank=Rank.ACE,   suit=Suit.HEARTS),
         Card(rank=Rank.KING,  suit=Suit.HEARTS),
         Card(rank=Rank.QUEEN, suit=Suit.HEARTS),
         Card(rank=Rank.JACK,  suit=Suit.HEARTS),
         Card(rank=Rank.TEN,   suit=Suit.HEARTS)
-    },
-    Suit.DIAMONDS: {
+    ),
+    Suit.DIAMONDS: (
         Card(rank=Rank.ACE,   suit=Suit.DIAMONDS),
         Card(rank=Rank.KING,  suit=Suit.DIAMONDS),
         Card(rank=Rank.QUEEN, suit=Suit.DIAMONDS),
         Card(rank=Rank.JACK,  suit=Suit.DIAMONDS),
         Card(rank=Rank.TEN,   suit=Suit.DIAMONDS)
-    },
-    Suit.CLUBS: {
+    ),
+    Suit.CLUBS: (
         Card(rank=Rank.ACE,   suit=Suit.CLUBS),
         Card(rank=Rank.KING,  suit=Suit.CLUBS),
         Card(rank=Rank.QUEEN, suit=Suit.CLUBS),
         Card(rank=Rank.JACK,  suit=Suit.CLUBS),
         Card(rank=Rank.TEN,   suit=Suit.CLUBS)
-    },
-    Suit.SPADES: {
+    ),
+    Suit.SPADES: (
         Card(rank=Rank.ACE,   suit=Suit.SPADES),
         Card(rank=Rank.KING,  suit=Suit.SPADES),
         Card(rank=Rank.QUEEN, suit=Suit.SPADES),
         Card(rank=Rank.JACK,  suit=Suit.SPADES),
         Card(rank=Rank.TEN,   suit=Suit.SPADES)
-    }
+    )
 }
 
 
@@ -79,25 +79,21 @@ class Hand:
 
     @staticmethod
     @cache
-    def _catalog(cards: tuple[Card]) -> tuple[HandRanking, tuple[Card], int]:
+    def _catalog(cards: tuple[Card]) -> tuple[HandRanking, tuple[Card, ...], int]:
         if (number_of_cards := len(cards)) == 0:
             raise ValueError
 
         at_least_five: bool = number_of_cards >= 5
 
         # Highest to lowest
-        # noinspection PyTypeChecker
-        by_value: tuple[Card] = tuple(sorted(cards, key=lambda card: card.rank.numeric_value, reverse=True))
+        by_value: tuple[Card, ...] = tuple(sorted(cards, key=lambda card: card.rank.numeric_value, reverse=True))
 
         if at_least_five:
             # 1. Royal flush
             for suit in Suit:
-                royal_flush_set: set = _ROYAL_FLUSHES[suit]
+                royal_flush: tuple = _ROYAL_FLUSHES[suit]
 
-                if all((card in cards for card in royal_flush_set)):
-                    royal_flush = tuple(sorted(royal_flush_set, key=lambda k: k.rank.numeric_value, reverse=True))
-
-                    # noinspection PyTypeChecker
+                if all((card in cards for card in royal_flush)):
                     return HandRanking.ROYAL_FLUSH, royal_flush, 0
 
             # 2. Straight flush
@@ -112,7 +108,6 @@ class Hand:
                         streak.append(this)
 
                         if len(streak) == 5:
-                            # noinspection PyTypeChecker
                             return HandRanking.STRAIGHT_FLUSH, tuple(streak), max_rank.numeric_value
 
                     else:
@@ -141,7 +136,6 @@ class Hand:
                 threes = tuple(filter(lambda card: card.rank.numeric_value == 12 - three_index, by_value))
                 twos   = tuple(filter(lambda card: card.rank.numeric_value == 12 - two_index  , by_value))
 
-                # noinspection PyTypeChecker
                 return HandRanking.FULL_HOUSE, threes + twos, (12-three_index) * 13 + (12-two_index)
 
             # 5. Flush
@@ -164,7 +158,6 @@ class Hand:
                 suit, value = reduce(lambda prev, next: prev if prev[1] > next[1] else next, suit_groups_values.items())
                 flush = suit_groups_filtered[suit]
 
-                # noinspection PyTypeChecker
                 return HandRanking.FLUSH, tuple(flush), flush[0].rank.numeric_value
 
             # 6. Straight
@@ -179,7 +172,6 @@ class Hand:
                         streak.append(this)
 
                         if len(streak) == 5:
-                            # noinspection PyTypeChecker
                             return HandRanking.STRAIGHT, tuple(streak), max_rank.numeric_value
 
                         break
@@ -190,7 +182,6 @@ class Hand:
         if three_index >= 0:
             threes = tuple(filter(lambda card: card.rank.numeric_value == 12 - three_index, by_value))
 
-            # noinspection PyTypeChecker
             return HandRanking.THREE_OF_A_KIND, threes, 12-three_index
 
         # 8. Two Pair
@@ -200,25 +191,22 @@ class Hand:
             twos        = tuple(filter(lambda card: card.rank.numeric_value == 12 - two_index       , by_value))
             second_twos = tuple(filter(lambda card: card.rank.numeric_value == 12 - second_two_index, by_value))
 
-            # noinspection PyTypeChecker
             return HandRanking.TWO_PAIR, twos + second_twos, (12-two_index) * 13 + (12-second_two_index)
 
         # 9. Pair
         if two_index >= 0:
             twos = tuple(filter(lambda card: card.rank.numeric_value == 12 - two_index, by_value))
 
-            # noinspection PyTypeChecker
             return HandRanking.PAIR, twos, 12-two_index
 
         # 10. High Card
         high_card: Card = reduce(lambda prev, next: prev if prev > next else next, cards)
         return HandRanking.HIGH_CARD, (high_card, ), high_card.rank.numeric_value
 
-    def _analyse(self) -> tuple[HandRanking, set[Card], int, tuple[Card]]:
+    def _analyse(self) -> tuple[HandRanking, tuple[Card, ...], int, tuple[Card, ...]]:
         ranking, cards, value = Hand._catalog(self.cards)
 
-        # noinspection PyTypeChecker
-        kickers: tuple[Card] = tuple(sorted((card for card in self.cards if card not in cards), key=lambda c: c.rank, reverse=True))
+        kickers: tuple[Card, ...] = tuple(sorted((card for card in self.cards if card not in cards), key=lambda c: c.rank, reverse=True))
 
         return ranking, cards, value, kickers
 
